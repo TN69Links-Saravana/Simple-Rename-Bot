006b389abe6b067d83119de1acc1a65035012e6f
TN69Links-Saravana <157959005+TN69Links-Saravana@users.noreply.github.com>
2024-03-03 14:45:23 +0530

    Update rename.py

--- main/rename.py
+++ main/rename.py
@@ -7,21 +7,21 @@ from main.utils import progress_message, humanbytes
 async def rename_file(bot, msg):
     reply = msg.reply_to_message
     if len(msg.command) < 2 or not reply:
-       return await msg.reply_text("Please Reply To An File or video or audio With filename + .extension eg:-(`.mkv` or `.mp4` or `.zip`)")
+       return await msg.reply_text("Pʟᴇᴀsᴇ Rᴇᴘʟʏ Tᴏ Aɴ Fɪʟᴇ Oʀ Vɪᴅᴇᴏ Oʀ Aᴜᴅɪᴏ Wɪᴛʜ FɪʟᴇNᴀᴍᴇ + .extension Exᴀᴍᴘʟᴇ :-(`.mkv` or `.mp4` or `.zip`)")
     media = reply.document or reply.audio or reply.video
     if not media:
-       await msg.reply_text("Please Reply To An File or video or audio With filename + .extension eg:-(`.mkv` or `.mp4` or `.zip`)")
+       await msg.reply_text("Pʟᴇᴀsᴇ Rᴇᴘʟʏ Tᴏ Aɴ Fɪʟᴇ Oʀ Vɪᴅᴇᴏ Oʀ Aᴜᴅɪᴏ Wɪᴛʜ FɪʟᴇNᴀᴍᴇ + .extension Exᴀᴍᴘʟᴇ :-(`.mkv` or `.mp4` or `.zip`)")
     og_media = getattr(reply, reply.media.value)
     new_name = msg.text.split(" ", 1)[1]
-    sts = await msg.reply_text("Trying to Downloading.....")
+    sts = await msg.reply_text("Tʀʏɪɴɢ Tᴏ Dᴏᴡɴʟᴏᴀᴅɪɴɢ.....")
     c_time = time.time()
-    downloaded = await reply.download(file_name=new_name, progress=progress_message, progress_args=("Download Started.....", sts, c_time)) 
+    downloaded = await reply.download(file_name=new_name, progress=progress_message, progress_args=("Dᴏᴡɴʟᴏᴀᴅ Sᴛᴀʀᴛᴇᴅ.....", sts, c_time)) 
     filesize = humanbytes(og_media.file_size)                
     if CAPTION:
         try:
             cap = CAPTION.format(file_name=new_name, file_size=filesize)
         except Exception as e:            
-            return await sts.edit(text=f"Your caption Error unexpected keyword ●> ({e})")           
+            return await sts.edit(text=f"Yᴏᴜʀ Cᴀᴘᴛɪᴏɴ Eʀʀᴏʀ Uɴᴇxᴘᴇᴄᴛᴇᴅ Kᴇʏᴡᴏʀᴅ ●> ({e})")           
     else:
         cap = f"{new_name}\n\n💽 size : {filesize}"
 
@@ -38,10 +38,10 @@ async def rename_file(bot, msg):
             print(e)        
             og_thumbnail = None
         
-    await sts.edit("Trying to Uploading")
+    await sts.edit("Tʀʏɪɴɢ Tᴏ Uᴘʟᴏᴀᴅɪɴɢ")
     c_time = time.time()
     try:
-        await bot.send_document(msg.chat.id, document=downloaded, thumb=og_thumbnail, caption=cap, progress=progress_message, progress_args=("Uploade Started.....", sts, c_time))        
+        await bot.send_document(msg.chat.id, document=downloaded, thumb=og_thumbnail, caption=cap, progress=progress_message, progress_args=("Uᴘʟᴏᴀᴅᴇ Sᴛᴀʀᴛᴇᴅ.....", sts, c_time))        
     except Exception as e:  
         return await sts.edit(f"Error {e}")                       
     try:
