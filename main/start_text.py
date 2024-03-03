ac57ae6311132d9be8c22045e7e8b97bf519a8ce
TN69Links-Saravana <157959005+TN69Links-Saravana@users.noreply.github.com>
2024-03-03 15:20:24 +0530

    Update start_text.py

--- main/start_text.py
+++ main/start_text.py
@@ -5,7 +5,7 @@ from config import ADMIN
 
 @Client.on_message(filters.command("start") & filters.private)                             
 async def start_cmd(bot, msg):
-    txt="Tʜɪs Is Pᴇʀsᴏɴᴀʟ Usᴇ Bᴏᴛ 🙏. Dᴏ Yᴏᴜ Wᴀɴᴛ Yᴏᴜʀ Oᴡɴ Bᴏᴛ? 👇 Cʟɪᴄᴋ Tʜᴇ Sᴏᴜʀᴄᴇ Cᴏᴅᴇ Tᴏ Dᴇᴘʟᴏʏ"
+    txt="<b>Tʜɪs Is Pᴇʀsᴏɴᴀʟ Usᴇ Bᴏᴛ 🙏. Dᴏ Yᴏᴜ Wᴀɴᴛ Yᴏᴜʀ Oᴡɴ Bᴏᴛ? 👇 Cʟɪᴄᴋ Tʜᴇ Sᴏᴜʀᴄᴇ Cᴏᴅᴇ Tᴏ Dᴇᴘʟᴏʏ</b>"
     btn = InlineKeyboardMarkup([[
         InlineKeyboardButton("🤖 Sᴏᴜʀᴄᴇ Cᴏᴅᴇ 🤖", url="https://github.com/TN69Links-Saravana/Simple-Rename-Bot")
         ],[
@@ -18,11 +18,11 @@ async def start_cmd(bot, msg):
 
 @Client.on_callback_query(filters.regex("start"))
 async def start(bot, msg, cb=True):   
-    txt=f"Hᴇʟʟᴏ {msg.from_user.mention} I Aᴍ Sɪᴍᴘʟᴇ Rᴇɴᴀᴍᴇ Bᴏᴛ Wɪᴛʜ Pᴇʀsᴏɴᴀʟ Usᴀɢᴇ.\nTʜɪs Bᴏᴛ Is Mᴀᴅᴇ Bʏ <b><a href=https://t.me/Cute_Boy_Saravana</a></b>"                                     
+    txt=f"Hᴇʟʟᴏ {msg.from_user.mention} <b>I Aᴍ Sɪᴍᴘʟᴇ Rᴇɴᴀᴍᴇ Bᴏᴛ Wɪᴛʜ Pᴇʀsᴏɴᴀʟ Usᴀɢᴇ.\nTʜɪs Bᴏᴛ Is Mᴀᴅᴇ Bʏ : <a href=https://t.me/Cute_Boy_Saravana</a></b>"                                     
     button= [[
         InlineKeyboardButton("🦋 Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ 🦋", url="https://t.me/TN69Links")
         ],[
-        InlineKeyboardButton("𖣘 Hᴇʟᴘ", callback_data="help"),
+        InlineKeyboardButton("<b>𖣘 Hᴇʟᴘ</b>", callback_data="help"),
         InlineKeyboardButton("✪⁠ Aʙᴏᴜᴛ", callback_data="about") 
     ]]  
     if cb:
@@ -33,12 +33,12 @@ async def start(bot, msg, cb=True):
 
 @Client.on_callback_query(filters.regex("help"))
 async def help(bot, msg):
-    txt = "Jᴜsᴛ Sᴇɴᴅ A Fɪʟᴇ Aɴᴅ /rename <new name> Wɪᴛʜ Rᴇᴘʟᴀʏᴇᴅ Yᴏᴜʀ Fɪʟᴇ\n\n"
+    txt = "<b>Jᴜsᴛ Sᴇɴᴅ A Fɪʟᴇ Aɴᴅ /rename <new name> Wɪᴛʜ Rᴇᴘʟᴀʏᴇᴅ Yᴏᴜʀ Fɪʟᴇ\n\n"
     txt += "Sᴇɴᴅ Pʜᴏᴛᴏ Tᴏ Sᴇᴛ Tʜᴜᴍʙɴᴀɪʟ Aᴜᴛᴏᴍᴀᴛɪᴄ \n"
     txt += "/view Tᴏ Sᴇᴇ Yᴏᴜʀ Tʜᴜᴍʙɴᴀɪʟ  \n"
-    txt += "/del Tᴏ Dᴇʟᴇᴛᴇ Yᴏᴜʀ Tʜᴜᴍʙɴᴀɪʟ"
+    txt += "/del Tᴏ Dᴇʟᴇᴛᴇ Yᴏᴜʀ Tʜᴜᴍʙɴᴀɪʟ</b>"
     button= [[        
-        InlineKeyboardButton("🚫 Cʟᴏsᴇ", callback_data="del"),
+        InlineKeyboardButton("<b>🚫 Cʟᴏsᴇ</b>", callback_data="del"),
         InlineKeyboardButton("⬅️ Bᴀᴄᴋ", callback_data="start") 
     ]]  
     await msg.message.edit(text=txt, reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview = True)
@@ -47,8 +47,8 @@ async def help(bot, msg):
 @Client.on_callback_query(filters.regex("about"))
 async def about(bot, msg):
     me=await bot.get_me()
-    Mᴀsᴛᴇʀ=f"<a href=https://t.me/Cute_Boy_Saravana>Ƭɴ69 ×͜× Sᴀʀᴀᴠᴀɴᴀ࿐</a> & <a href=https://t.me/TN69Vikram>ᥫ᭡፝֟፝֟ Vikram࿐</a>"  
-    Sᴏᴜʀᴄᴇ="<a href=https://github.com/TN69Links-Saravana/Simple-Rename-Bot>Click Here</a>"
+    Mᴀsᴛᴇʀ =f"<a href=https://t.me/Cute_Boy_Saravana>Ƭɴ69 ×͜× Sᴀʀᴀᴠᴀɴᴀ࿐</a> & <a href=https://t.me/TN69Vikram>ᥫ᭡፝֟፝֟ Vikram࿐</a>"  
+    Sᴏᴜʀᴄᴇ ="<a href=https://github.com/TN69Links-Saravana/Simple-Rename-Bot>Click Here</a>"
     txt=f"<b>Bᴏᴛ Nᴀᴍᴇ : {me.mention}\nDᴇᴠᴇʟᴏᴘᴇʀ: <a href=https://t.me/TN69Links</a>\n🌸 YᴏᴜTᴜʙᴇ Cʜᴀɴɴᴇʟ 🌸: <a href=https://youtube.com/@TN69Links>Sᴜʙsᴄʀɪʙᴇ Nᴏᴡ</a>\n😈 Mʏ Mᴀsᴛᴇʀ's: {Master}\nSᴏᴜʀᴄᴇ Cᴏᴅᴇ : {Source}</b>"                 
     button= [[        
         InlineKeyboardButton("🚫 Cʟᴏsᴇ", callback_data="del"),
