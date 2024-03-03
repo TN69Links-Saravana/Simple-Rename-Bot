40ce9e47ad8322b14c6f7aa97dde937489423922
TN69Links-Saravana <157959005+TN69Links-Saravana@users.noreply.github.com>
2024-03-03 14:25:49 +0530

    Update start_text.py

--- main/start_text.py
+++ main/start_text.py
@@ -5,11 +5,11 @@ from config import ADMIN
 
 @Client.on_message(filters.command("start") & filters.private)                             
 async def start_cmd(bot, msg):
-    txt="This is personal use bot 🙏. Dᴏ Yᴏᴜ Wᴀɴᴛ Yᴏᴜʀ Oᴡɴ Bᴏᴛ? 👇 Cʟɪᴄᴋ Tʜᴇ Sᴏᴜʀᴄᴇ Cᴏᴅᴇ Tᴏ Dᴇᴘʟᴏʏ"
+    txt="Tʜɪs Is Pᴇʀsᴏɴᴀʟ Usᴇ Bᴏᴛ 🙏. Dᴏ Yᴏᴜ Wᴀɴᴛ Yᴏᴜʀ Oᴡɴ Bᴏᴛ? 👇 Cʟɪᴄᴋ Tʜᴇ Sᴏᴜʀᴄᴇ Cᴏᴅᴇ Tᴏ Dᴇᴘʟᴏʏ"
     btn = InlineKeyboardMarkup([[
-        InlineKeyboardButton("🤖 Sᴏᴜʀᴄᴇ Cᴏᴅᴇ", url="https://github.com/TN69Links-Saravana/Simple-Rename-Bot")
+        InlineKeyboardButton("🤖 Sᴏᴜʀᴄᴇ Cᴏᴅᴇ 🤖", url="https://github.com/TN69Links-Saravana/Simple-Rename-Bot")
         ],[
-        InlineKeyboardButton("🖥️ Hᴏᴡ Tᴏ Dᴇᴘʟᴏʏ", url="https://youtube.com/@TN69Links")
+        InlineKeyboardButton("🖥️ Hᴏᴡ Tᴏ Dᴇᴘʟᴏʏ 🖥️", url="https://youtube.com/@TN69Links")
     ]])
     if msg.from_user.id != ADMIN:
         return await msg.reply_text(text=txt, reply_markup=btn, disable_web_page_preview = True)
@@ -22,8 +22,8 @@ async def start(bot, msg, cb=True):
     button= [[
         InlineKeyboardButton("🦋 Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ 🦋", url="https://t.me/TN69Links")
         ],[
-        InlineKeyboardButton("ℹ️ Help", callback_data="help"),
-        InlineKeyboardButton("📡 About", callback_data="about") 
+        InlineKeyboardButton("𖣘 Hᴇʟᴘ", callback_data="help"),
+        InlineKeyboardButton("✪⁠ Aʙᴏᴜᴛ", callback_data="about") 
     ]]  
     if cb:
         await msg.message.edit(text=txt, reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview = True, parse_mode=enums.ParseMode.HTML)
@@ -38,8 +38,8 @@ async def help(bot, msg):
     txt += "/view to see your thumbnail \n"
     txt += "/del to delete your thumbnail"
     button= [[        
-        InlineKeyboardButton("🚫 Close", callback_data="del"),
-        InlineKeyboardButton("⬅️ Back", callback_data="start") 
+        InlineKeyboardButton("🚫 Cʟᴏsᴇ", callback_data="del"),
+        InlineKeyboardButton("⬅️ Bᴀᴄᴋ", callback_data="start") 
     ]]  
     await msg.message.edit(text=txt, reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview = True)
 
@@ -47,12 +47,12 @@ async def help(bot, msg):
 @Client.on_callback_query(filters.regex("about"))
 async def about(bot, msg):
     me=await bot.get_me()
-    Mᴀsᴛᴇʀ=f"<a href=https://t.me/Cute_Boy_Saravana>Ƭɴ69 ×͜× Sᴀʀᴀᴠᴀɴᴀ࿐</a> & <a href=https://t.me/TN69Vikram>ᥫ᭡፝֟፝֟ Vikram࿐</a>"  
-    Sᴏᴜʀᴄᴇ="<a href=https://github.com/TN69Links-Saravana/Simple-Rename-Bot>Click Here</a>"
-    txt=f"<b>Bᴏᴛ Nᴀᴍᴇ: {me.mention}\nDᴇᴠᴇʟᴏᴘᴇʀ: <a href=https://t.me/TN69Links</a>\n🦋 Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ 🦋: <a href=https://youtube.com/@TN69Links>🌸 YᴏᴜTᴜʙᴇ Cʜᴀɴɴᴇʟ 🌸</a>\n😈 Mʏ Mᴀsᴛᴇʀ's 😈: {Master}\nSource Code: {Source}</b>"                 
+    Master=f"<a href=https://t.me/Cute_Boy_Saravana>Ƭɴ69 ×͜× Sᴀʀᴀᴠᴀɴᴀ࿐</a> & <a href=https://t.me/TN69Vikram>ᥫ᭡፝֟፝֟ Vikram࿐</a>"  
+    Source="<a href=https://github.com/TN69Links-Saravana/Simple-Rename-Bot>Click Here</a>"
+    txt=f"<b>Bot Name: {me.mention}\nDeveloper: <a href=https://t.me/TN69Links</a>\n🦋 Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ 🦋: <a href=https://youtube.com/@TN69Links>🌸 YᴏᴜTᴜʙᴇ Cʜᴀɴɴᴇʟ 🌸</a>\n😈 Mʏ Mᴀsᴛᴇʀ's 😈: {Master}\nSource Code: {Source}</b>"                 
     button= [[        
-        InlineKeyboardButton("🚫 Close", callback_data="del"),
-        InlineKeyboardButton("⬅️ Back", callback_data="start") 
+        InlineKeyboardButton("🚫 Cʟᴏsᴇ", callback_data="del"),
+        InlineKeyboardButton("⬅️ Bᴀᴄᴋ", callback_data="start") 
     ]]  
     await msg.message.edit(text=txt, reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview = True, parse_mode=enums.ParseMode.HTML)
 
