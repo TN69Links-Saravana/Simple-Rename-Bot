4fd0b7b7063c053e94fd342d613acd0322f9549b
TN69Links-Saravana <157959005+TN69Links-Saravana@users.noreply.github.com>
2024-03-03 12:38:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,7 @@ API_ID = int(environ.get("API_ID", "24482734"))
 API_HASH = environ.get("API_HASH", "5ccf6a58166cc047a7eba01c5dbc930c")
 BOT_TOKEN = environ.get("BOT_TOKEN", "6433110243:AAEHTsio1VnTpliE78Z02AzImHJFrIM6VYQ")
 ADMIN = int(environ.get("ADMIN", "1790775725"))          
-CAPTION = environ.get("CAPTION", "{file_name}
-
-💽 Size = {file_size}
-
-📯Join My Channel : @TN69Links")
+CAPTION = environ.get("CAPTION", "@TN69Links")
 
 # for thumbnail ( back end is TN69Links brain 😉)
 DOWNLOAD_LOCATION = "./DOWNLOADS"
