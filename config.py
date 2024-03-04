442e6c9a763a87b866a6d9be622b788df02a8d56
TN69Links-Saravana <157959005+TN69Links-Saravana@users.noreply.github.com>
2024-03-04 10:43:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,6 @@ API_HASH = environ.get("API_HASH", "5ccf6a58166cc047a7eba01c5dbc930c")
 BOT_TOKEN = environ.get("BOT_TOKEN", "6433110243:AAEHTsio1VnTpliE78Z02AzImHJFrIM6VYQ")
 ADMIN = int(environ.get("ADMIN", "1790775725"))          
 CAPTION = environ.get("CAPTION", "<b>{file_name}
-💽 Size = {file_size}
-
 ➥ ❤️Jᴏɪɴ : @RoyalxMovies</b>")
 
 # for thumbnail ( back end is TN69Links brain 😉)
